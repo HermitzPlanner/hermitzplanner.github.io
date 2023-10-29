9fb0cad9b0332fa3b545986a4334515ca75ad5c4
HermitzPlanner <nightbugh@gmail.com>
2023-10-29 10:39:35 -0300

    update

--- js/galleryfetch.js
+++
@@ -1,9 +0,0 @@
-async function galleryfetch() {
-    const skinResponse = await fetch('json/skins.json'); // grab json
-    const skinData = await skinResponse.json(); // wait for json and make it data
-    skinData.forEach(function (skinJson) {
-        const galleryIcon = document.getElementById(`gallery_icon_${skinJson.id}`)
-        galleryIcon.src = `https://raw.githubusercontent.com/HermitzPlanner/planner-images/main/icon/${skinJson.id}.png`
-
-    })
-}
